6fdb287e71413d0bf9708ae655eb587e4ac7df3b
mnwtky <69100365+mnwtky@users.noreply.github.com>
2021-11-20 17:16:49 +0900

    打刻機能の実装

--- attendance/Models/Attendance.cs
+++ attendance/Models/Attendance.cs
@@ -1,13 +1,45 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
 namespace attendance.Models
 {
     public class Attendance
     {
+        //ID
         public int ID { get; set; }
-        public string WorkingDate { get; set; }
+
+        //勤務日
+        [Display(Name = "勤務日")]
+        [Required(ErrorMessage = "必須入力です")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime WorkingDate { get; set; }
+
+        //始業時間
+        [Display(Name = "始業時間")]
+        [Required(ErrorMessage = "必須入力です")]
+        [DataType(DataType.Time)]
+        [DisplayFormat(DataFormatString = "{0:H:mm}", ApplyFormatInEditMode = true)]
         public DateTime ClonkingIn { get; set; }
+
+        //終業時間
+        [Display(Name = "終業時間")]
+        [Required(AllowEmptyStrings = true)]
+        [DataType(DataType.Time)]
+        [DisplayFormat(DataFormatString = "{0:H:mm}", ApplyFormatInEditMode = true)]
         public DateTime ClockingOut { get; set; }
-        public DateTime BreakeTime { get; set; }
-        public DateTime WorkingTime { get; set; }
+
+        //休憩時間
+        [Display(Name = "休憩時間")]
+        [Required(AllowEmptyStrings = true)]
+        public decimal BreakeTime { get; set; }
+
+        //実働時間
+        [Display(Name = "実働時間")]
+        [Required(AllowEmptyStrings = true)]
+        public decimal WorkingTime { get; set; }
     }
 }
